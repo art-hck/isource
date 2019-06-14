581579114844077d52a8db8d0512f174102f3330
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-06-14 10:09:10 +0300

    gpn_market-45

--- www/src/app/registration/registration.component.ts
+++ www/src/app/registration/registration.component.ts
@@ -7,44 +7,52 @@ import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, 
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  registrationForm: FormGroup;
+  userForm: FormGroup;
   item: boolean = false;
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-    this.registrationForm = this.formBuilder.group({
-      login: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+    // this.registrationForm = this.formBuilder.group({
+    //   login: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+    //   passwords: this.formBuilder.group({
+    //     password: ['', [Validators.required]],
+    //     confirmedPassword: ['', [Validators.required]],
+    //   }, {validator: this.passwordConfirming}),
+    //   quantity: [null, [Validators.required, Validators.pattern(/^[+]?[1-9]\d*$/)]],
+    //   fio: [''],
+    //   deliveryBasis: ['', [Validators.required]],
+    //
+    // });
+    this.userForm = this.formBuilder.group({
       passwords: this.formBuilder.group({
         password: ['', [Validators.required]],
         confirmedPassword: ['', [Validators.required]],
       }, {validator: this.passwordConfirming}),
-      quantity: [null, [Validators.required, Validators.pattern(/^[+]?[1-9]\d*$/)]],
-      fio: [''],
-      deliveryBasis: ['', [Validators.required]],
 
     });
-
   }
 
   isFieldValid(field: string) {
-    return this.registrationForm.get(field).errors
-      && (this.registrationForm.get(field).touched || this.registrationForm.get(field).dirty);
+    return this.userForm.get(field).errors
+      && (this.userForm.get(field).touched || this.userForm.get(field).dirty);
+    console.log(this.userForm.get(field).errors
+      && (this.userForm.get(field).touched || this.userForm.get(field).dirty));
   }
 
-  // passwordConfirming(c: AbstractControl): { invalid: boolean } {
-  //   if (c.get('password').value !== c.get('confirmedPassword').value) {
-  //     return {invalid: true};
-  //   }
-  // }
-
-  passwordConfirming(): ValidatorFn {
-    return (c: AbstractControl): ValidationErrors | null => {
-    return (c.get('password').value !== c.get('confirmedPassword').value) ? null : {
-      invalid: true
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('confirmedPassword').value) {
+      return {invalid: true};
     }
   }
-}
+
+//   passwordConfirming(): ValidatorFn {
+//     return (c: AbstractControl): ValidationErrors | null => {
+//     return (c.get('password').value !== c.get('confirmedPassword').value) ? null : {
+//       invalid: true
+//     }
+//   }
+// }
 
 }
