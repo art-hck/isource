4aa905833613ed83108f62603351c625c34f18b6
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-07-01 14:35:04 +0300

    gpn_market-45 Исправление замечаний

--- www/src/app/request/request.module.ts
+++ www/src/app/request/request.module.ts
@@ -12,6 +12,7 @@ import { RequestService as CustomerRequestService } from "./customer/services/re
 import { RequestListComponent } from "./common/components/request-list/request-list.component";
 import { DocumentListComponent } from './common/components/document-list/document-list.component';
 import { DocumentUploadListComponent } from "./common/components/document-upload-list/document-upload-list.component";
+import { OffersComponent as BackofficeOffersComponent } from './back-office/components/offers/offers.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { DocumentUploadListComponent } from "./common/components/document-upload
     DocumentUploadListComponent,
     BackofficeRequestViewComponent,
     CustomerRequestViewComponent,
-    DocumentListComponent
+    DocumentListComponent,
+    BackofficeOffersComponent
   ],
   imports: [
     SharedModule,
