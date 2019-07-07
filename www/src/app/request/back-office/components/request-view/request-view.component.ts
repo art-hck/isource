951a066bb6dc8e46640d118a4139ba62be32db8a
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-07-07 14:34:03 +0300

    gpn_market-132 редизайн

--- www/src/app/request/back-office/components/request-view/request-view.component.ts
+++ www/src/app/request/back-office/components/request-view/request-view.component.ts
@@ -5,9 +5,6 @@ import {RequestService} from "../../services/request.service";
 import {Request} from "../../../common/models/request";
 import {RequestPosition} from "../../../common/models/request-position";
 import {RequestPositionWorkflowStepLabels} from "../../../common/dictionaries/request-position-workflow-step-labels";
-import {OffersService} from "../../services/offers.service";
-import {RequestPositionWorkflowSteps} from "../../../common/enum/request-position-workflow-steps";
-import {RequestTypesLabels} from "../../../common/dictionaries/request-types-labels";
 import {RequestTypes} from "../../../common/enum/request-types";
 
 @Component({
@@ -20,14 +17,19 @@ export class RequestViewComponent implements OnInit {
   requestId: Uuid;
   request: Request;
   requestPositions: RequestPosition[];
+  selectedRequestPosition: RequestPosition;
+
+  showInfo = false;
+  showRequestInfo: boolean;
+  showPositionList = true;
+  selectedIndex: number;
   requestPositionWorkflowStepLabels = Object.entries(RequestPositionWorkflowStepLabels);
 
   expanded = false;
 
   constructor(
     private route: ActivatedRoute,
-    private requestService: RequestService,
-    private offersService: OffersService
+    private requestService: RequestService
   ) {
   }
 
@@ -49,37 +51,19 @@ export class RequestViewComponent implements OnInit {
     this.expanded = !this.expanded;
   }
 
-  onChangeStatus(requestPosition: RequestPosition, newStatus: string) {
-    this.requestService.changeStatus(this.requestId, requestPosition.id, newStatus).subscribe(
-      (data: any) => {
-        requestPosition.status = data.status;
-        requestPosition.statusLabel = data.statusLabel;
-        if (data.requestStatus !== null) {
-          this.requestService.getRequestInfo(this.requestId).subscribe(
-            (request: Request) => {
-              this.request = request;
-            }
-          );
-        }
-      });
-  }
-
-  onPublishOffers(requestPosition: RequestPosition) {
-    this.offersService.publishOffers(this.requestId, requestPosition.id).subscribe(
-      (data: any) => {
-        requestPosition.status = data.status;
-        requestPosition.statusLabel = data.statusLabel;
-      }
-    );
-  }
-
-  canPublish(requestPosition: RequestPosition) {
-    return (requestPosition.status === RequestPositionWorkflowSteps.PROPOSALS_PREPARATION
-        || requestPosition.status === RequestPositionWorkflowSteps.NEW) && (requestPosition.linkedOffers.length !== 0);
-  }
-
   checkIfFreeForm() {
     return this.request.type === RequestTypes.FREE_FORM;
   }
 
+  onSelectPosition(requestPosition: RequestPosition, i: number) {
+    this.selectedRequestPosition = requestPosition;
+    this.showInfo = true;
+    this.showRequestInfo = false
+    this.selectedIndex = i;
+  }
+
+  onSelectRequest() {
+    this.showRequestInfo = true;
+    this.showInfo = false
+  }
 }
