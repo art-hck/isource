3d9a1fc2c1cc11d810f8861c420f8db6cf8ce611
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-07-08 10:38:11 +0300

    gpn_market-132 Редизайн

---
+++ www/src/app/request/back-office/components/back-office-request-view/back-office-request-view.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {Request} from "../../../common/models/request";
+import {RequestPosition} from "../../../common/models/request-position";
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../../services/request.service";
+import {Uuid} from "../../../../cart/models/uuid";
+
+@Component({
+  selector: 'app-back-office-request-view',
+  templateUrl: './back-office-request-view.component.html',
+  styleUrls: ['./back-office-request-view.component.css']
+})
+export class BackOfficeRequestViewComponent implements OnInit {
+  requestId: Uuid;
+  request: Request;
+  requestPositions: RequestPosition[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private requestService: RequestService
+  ) {
+  }
+
+  ngOnInit() {
+    this.requestId = this.route.snapshot.paramMap.get('id');
+    this.requestService.getRequestInfo(this.requestId).subscribe(
+      (request: Request) => {
+        this.request = request;
+      }
+    );
+    this.requestService.getRequestPositions(this.requestId).subscribe(
+      (requestPositions: RequestPosition[]) => {
+        this.requestPositions = requestPositions;
+      }
+    );
+  }
+}
