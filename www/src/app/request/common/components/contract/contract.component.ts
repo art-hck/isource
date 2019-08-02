99474f1d648ec4e6186cfe3a2a399c1fec637b9e
alexander <a.kazorin@etpgpb.ru>
2019-08-02 12:02:12 +0300

    gpn_market-271 Загрузка Договора без комментария

--- www/src/app/request/common/components/contract/contract.component.ts
+++ www/src/app/request/common/components/contract/contract.component.ts
@@ -83,4 +83,14 @@ export class ContractComponent implements OnChanges, OnInit {
   afterGetContract(data: any) {
     this.requestContract = data;
   }
+
+  isShowCommentsColumn(): boolean {
+    for (const document of this.requestContract.documents) {
+      if (document.comments) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
