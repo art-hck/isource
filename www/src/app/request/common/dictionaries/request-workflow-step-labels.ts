d7764a58a719063d5c495edfd83f075b09f039cb
alexander <a.kazorin@etpgpb.ru>
2019-08-26 13:45:10 +0300

    gpn_market-353 Сотрудник бекофиса может отправить черновики на согласование заказчику

--- www/src/app/request/common/dictionaries/request-workflow-step-labels.ts
+++ www/src/app/request/common/dictionaries/request-workflow-step-labels.ts
@@ -3,6 +3,7 @@ import { RequestWorkflowSteps } from "../enum/request-workflow-steps";
 const labels: {[key: string]: string} = {};
 
 labels[RequestWorkflowSteps.DRAFT] = 'Черновик';
+labels[RequestWorkflowSteps.ON_CUSTOMER_APPROVAL] = 'На согласовании заказчика';
 labels[RequestWorkflowSteps.NEW] = 'Новая';
 labels[RequestWorkflowSteps.IN_PROGRESS] = 'В обработке';
 labels[RequestWorkflowSteps.COMPLETED] = 'Выполнена';
