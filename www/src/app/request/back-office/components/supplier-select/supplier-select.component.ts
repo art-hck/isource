138b20eb379f06328051f1f73326032fefbb6c99
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-09-20 12:51:21 +0300

    gpn_market-451 Отображение к то и когда принял решение по тп

---
+++ www/src/app/request/back-office/components/supplier-select/supplier-select.component.ts
@@ -0,0 +1,53 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ContragentList} from "../../../../contragent/models/contragent-list";
+import {ContragentService} from "../../../../contragent/services/contragent.service";
+
+@Component({
+  selector: 'app-supplier-select',
+  templateUrl: './supplier-select.component.html',
+  styleUrls: ['./supplier-select.component.scss']
+})
+export class SupplierSelectComponent implements OnInit {
+  contragentForm: FormGroup;
+  contragents: ContragentList[];
+  showContragentList = false;
+
+  @Output() selectedContragent = new EventEmitter<ContragentList>();
+  @Output() showContragentInfo = new EventEmitter<boolean>();
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private getContragentService: ContragentService
+  ) { }
+
+  ngOnInit() {
+    this.contragentForm = this.formBuilder.group({
+      searchContragent: [null, Validators.required]
+    });
+    this.getContragentList();
+  }
+
+  getContragentList(): void {
+    this.getContragentService.getContragentList().subscribe(
+      (data: ContragentList[]) => {
+        this.contragents = data;
+      });
+  }
+
+  getSearchValue() {
+    return this.contragentForm.value.searchContragent;
+  }
+
+  onShowContragentList() {
+    this.showContragentList = !this.showContragentList;
+  }
+
+  selectContragent(contragent: ContragentList) {
+    this.contragentForm.patchValue({"searchContragent": contragent.shortName});
+    this.showContragentList = false;
+    this.selectedContragent.emit(contragent);
+    this.showContragentInfo.emit(true);
+  }
+}
