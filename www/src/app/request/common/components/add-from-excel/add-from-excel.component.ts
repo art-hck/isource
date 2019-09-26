f8e14201765cf7d0ecceda065b91a55f827f20e3
Karen <id12102180@ya.ru>
2019-09-26 13:17:53 +0300

    gpn_market-466 Правка по ревью

--- www/src/app/request/common/components/add-from-excel/add-from-excel.component.ts
+++ www/src/app/request/common/components/add-from-excel/add-from-excel.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 })
 export class AddFromExcelComponent implements OnInit {
 
-  @Output() submit = new EventEmitter<object>();
+  @Output() submit = new EventEmitter<{ files: File[], requestName: string }>();
   @Input() templateUrl: string;
 
   requestName = "";
