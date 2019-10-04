5547f790755f88c510f44dbaeb5ff750c5a8db8d
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-10-04 12:49:13 +0300

    gpn_market-504 Создание списка РКД

--- www/src/app/request/back-office/services/design-documentation.service.ts
+++ www/src/app/request/back-office/services/design-documentation.service.ts
@@ -1,6 +1,8 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Uuid} from "../../../cart/models/uuid";
+import {RequestPosition} from "../../common/models/request-position";
+import {DesignDocumentation} from "../../common/models/design-documentation";
 
 @Injectable()
 export class DesignDocumentationService {
@@ -13,5 +15,22 @@ export class DesignDocumentationService {
   getDesignDocumentationList(requestId: Uuid) {
     const url = `requests/${requestId}/designs`;
     return this.api.get(url);
+  }
+
+  getPositionList(requestId: Uuid) {
+    const url = `requests/${requestId}/designs/positions`;
+    return this.api.get(url);
+  }
+
+  addDesignDocumentationList(id: Uuid, designDocs: DesignDocumentation[], positions: RequestPosition[]) {
+    const url = `requests/${id}/designs/create`;
+    const ids = [];
+    for (const position of positions) {
+      ids.push(position.id);
+    }
+    return this.api.post(url, {
+      positions: ids,
+      designDocs: designDocs
+    });
   }
 }
