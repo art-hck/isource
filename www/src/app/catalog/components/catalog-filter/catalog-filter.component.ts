e941973ae21f564c6f8bda7ebaf0813157ce3585
a.baydin <a.baydin@etpgpb.ru>
2019-11-08 12:55:48 +0300

    gpn_market-706 Временно отключены основные фильтра

--- www/src/app/catalog/components/catalog-filter/catalog-filter.component.ts
+++ www/src/app/catalog/components/catalog-filter/catalog-filter.component.ts
@@ -14,11 +14,11 @@ import { CatalogCategoryFilter } from "../../models/catalog-category-filter";
 
 export class CatalogFilterComponent implements OnInit, OnDestroy {
   @Output() filter = new EventEmitter<CatalogCategoryFilter>();
-  private subscription: Subscription;
+  private subscription: Subscription = new Subscription();
 
   public form = new FormGroup({
-    'availability': new FormControl(true),
-    'price': new FormControl([0, 1000]),
+    'availability': new FormControl({value: true, disabled: true}),
+    'price': new FormControl({value: [0, 1000], disabled: true}),
     'attributes': new FormArray([])
   });
 
@@ -33,36 +33,42 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.subscription = this.route.params.pipe(
-      // Только если есть categoryId
-      filter(routeParams => routeParams.categoryId),
-      // Только после получения categoryId получаем аттрибуты категории
-      flatMap(routeParams => this.catalogService.getCategoryAttributes(routeParams.categoryId)),
-      // Проставляем их в форме
-      tap(attributes => {
-        attributes.forEach(attribute => {
-          this.attributes.push(new FormGroup({
-            'label': new FormControl(attribute.name),
-            'values': new FormArray(attribute.values.map(
-              attributeValue => new FormGroup({
-                'id': new FormControl(attributeValue.id),
-                'value': new FormControl(attributeValue.value),
-                'checked': new FormControl(true)
-              })))
-          }));
-        });
-      }),
-      // После того как проинициализировали форму, подписываемся на её изменения
-      switchMap(() => this.form.valueChanges),
-      // Пропускаем изменения которые чаще 500ms для разгрузки бэкенда
-      debounceTime(500),
-      filter(() => this.form.valid)
-    ).subscribe(() => this.submit());
+    this.subscription.add(
+      this.route.params.pipe(
+        // Только если есть categoryId
+        filter(routeParams => routeParams.categoryId),
+        // Только после получения categoryId получаем аттрибуты категории
+        flatMap(routeParams => this.catalogService.getCategoryAttributes(routeParams.categoryId)),
+        // Проставляем их в форме
+        tap(attributes => {
+          attributes.forEach(attribute => {
+            this.attributes.push(new FormGroup({
+              'label': new FormControl(attribute.name),
+              'values': new FormArray(attribute.values.map(
+                attributeValue => new FormGroup({
+                  'id': new FormControl(attributeValue.id),
+                  'value': new FormControl(attributeValue.value),
+                  'checked': new FormControl(true)
+                })))
+            }));
+          });
+        }),
+        // После того как проинициализировали форму, подписываемся на её изменения
+        switchMap(() => this.form.valueChanges),
+        // Пропускаем изменения которые чаще 500ms для разгрузки бэкенда
+        debounceTime(500),
+        filter(() => this.form.valid)
+      ).subscribe(() => this.submit())
+    );
   }
 
   public submit(): void {
     const attributes = this.form.value.attributes
+      // Оставляем только те аттрибуты, у которых не все значения выделены чекбоксами
+      .filter(attribute => attribute.values.length > attribute.values.filter((value) => value.checked).length)
+      // Оставляем только массивы с id значений аттрибутов
       .map(attribute => attribute.values.filter((value) => value.checked).map(value => value.id))
+      // Преобразуем в единый массив
       .reduce((prev, curr) => [...prev, ...curr], []);
 
     this.filter.emit({attributes});
