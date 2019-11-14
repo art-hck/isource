e40b2e3954bc167582be2c7b67980eddb1e5a698
Gnedov Pavel <pavel@gnedov.info>
2019-11-14 23:58:25 +0700

    gpn_market-721 Ограничение на операции с коммеческими предложениями позиций с не правильным статусом

--- www/src/app/request/back-office/components/add-offers/add-offers.component.ts
+++ www/src/app/request/back-office/components/add-offers/add-offers.component.ts
@@ -145,7 +145,10 @@ export class AddOffersComponent implements OnInit {
   }
 
   positionCanBeSelected(requestPosition: RequestPosition): boolean {
-    return (requestPosition.linkedOffers.length !== 0 && !this.positionIsSentForAgreement(requestPosition));
+    return (
+      requestPosition.linkedOffers.length !== 0 &&
+      requestPosition.status === RequestPositionWorkflowSteps.PROPOSALS_PREPARATION
+    );
   }
 
   positionIsSentForAgreement(requestPosition: RequestPosition): boolean {
@@ -243,7 +246,10 @@ export class AddOffersComponent implements OnInit {
   }
 
   isOfferClickable(requestPosition: RequestPosition): boolean {
-    return !this.positionIsSentForAgreement(requestPosition);
+    return (
+      !this.positionIsSentForAgreement(requestPosition) &&
+      requestPosition.status === RequestPositionWorkflowSteps.PROPOSALS_PREPARATION
+    );
   }
 
   onAddOffer(): void {
