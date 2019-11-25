b7e2c2a0ced4062c799c47848834fa7d4b3834f0
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-11-25 12:24:23 +0300

    gpn_market-813 Отображение фильтра списка заявок в модальном окне при разрешении менее 1440px

---
+++ www/src/app/request/common/components/request-list/request-list-filter/request-list-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-list-filter',
+  templateUrl: './request-list-filter.component.html',
+  styleUrls: ['./request-list-filter.component.scss']
+})
+export class RequestListFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
