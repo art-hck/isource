29cb8a2ab6d2f8f263426f4b3b2d317a3d842510
Karen <id12102180@ya.ru>
2019-12-04 12:59:08 +0300

    gpn_market-893 Редизайн страницы авторизации

--- www/src/app/login/login.component.ts
+++ www/src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { CustomValidators } from "../shared/forms/custom.validators";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   authForm: FormGroup;
