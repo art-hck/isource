6ebe565991013e44bda8e3559b8a3bcf685295ea
a.baydin <a.baydin@etpgpb.ru>
2019-12-12 21:08:58 +0300

    gpn_market-887 Вывод документов с группировкой по дате

--- www/src/app/request/common/components/request-position/request-position.component.ts
+++ www/src/app/request/common/components/request-position/request-position.component.ts
@@ -1,24 +1,45 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RequestPosition } from "../../models/request-position";
 import { RequestPositionStatusService } from "../../services/request-position-status.service";
 import { RequestPositionWorkflowSteps } from "../../enum/request-position-workflow-steps";
 import { UserInfoService } from "../../../../auth/services/user-info.service";
 import { Uuid } from "../../../../cart/models/uuid";
+import { RequestDocument } from "../../models/request-document";
+import * as moment from "moment";
 
 @Component({
   selector: 'app-request-position',
   templateUrl: './request-position.component.html',
   styleUrls: ['./request-position.component.scss']
 })
-export class RequestPositionComponent {
+export class RequestPositionComponent implements OnInit {
   @Input() requestId: Uuid;
   @Input() position: RequestPosition;
   @Input() statuses: [string, string][];
   @Output() changeStatus = new EventEmitter<{ status, position }>();
+  datesWithDocuments: DateWithDocuments[];
 
   constructor(private positionStatusService: RequestPositionStatusService, public user: UserInfoService) {}
+
+  ngOnInit() {
+    this.datesWithDocuments = this.position.documents.reduce(
+      (arr: DateWithDocuments[], document: RequestDocument) => {
+        const date = moment(document.created).locale("ru").format('DD MMMM YYYY');
+        let i = arr.findIndex(_item => _item.date === date);
+
+        if (i < 0) {
+          i = arr.push({date, documents: []}) - 1;
+        }
+
+        arr[i].documents.push(document);
+        return arr;
+      }, []
+    );
+  }
 
   isAfterManufacturing(position: RequestPosition): boolean {
     return this.positionStatusService.isStatusAfter(position.status, RequestPositionWorkflowSteps.MANUFACTURING);
   }
 }
+
+export class DateWithDocuments { date: string; documents: RequestDocument[]; }
