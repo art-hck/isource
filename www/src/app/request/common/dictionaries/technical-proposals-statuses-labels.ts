888f5a694a562799055a5becc5117a26964b1ebe
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2019-12-25 14:06:17 +0300

    gpn_market-973 Разработка компонента элемента списка ТП

--- www/src/app/request/common/dictionaries/technical-proposals-statuses-labels.ts
+++ www/src/app/request/common/dictionaries/technical-proposals-statuses-labels.ts
@@ -2,8 +2,8 @@ import { TechnicalProposalsStatuses } from "../enum/technical-proposals-statuses
 
 const labels: {[key: string]: string} = {};
 
-labels[TechnicalProposalsStatuses.NEW] = 'Подготовка предложения';
-labels[TechnicalProposalsStatuses.SENT_TO_REVIEW] = 'На согласовании заказчика';
+labels[TechnicalProposalsStatuses.NEW] = 'Черновик';
+labels[TechnicalProposalsStatuses.SENT_TO_REVIEW] = 'Решение не принято';
 labels[TechnicalProposalsStatuses.ACCEPTED] = 'Согласовано';
 
 export const TechnicalProposalsStatusesLabels: {[key: string]: string} = labels;
