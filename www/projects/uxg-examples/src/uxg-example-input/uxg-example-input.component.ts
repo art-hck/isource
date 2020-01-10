813c5fc4287a4909abc6e3b202ba283357683dce
a.baydin <a.baydin@etpgpb.ru>
2020-01-10 18:18:58 +0300

    gpn_market-888 Фикс скролла dropdown компонента

--- www/projects/uxg-examples/src/uxg-example-input/uxg-example-input.component.ts
+++ www/projects/uxg-examples/src/uxg-example-input/uxg-example-input.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { UxgPopoverContentDirection } from "../../../uxg/src/components/uxg-popover/uxg-popover-direction-enum";
 
 @Component({
   selector: 'uxg-example-input',
@@ -7,37 +8,66 @@ import { FormControl, FormGroup, ValidationErrors, Validators } from "@angular/f
 })
 export class UxgExampleInputComponent {
   readonly example = `<form [formGroup]="form" (ngSubmit)="submit(form)">
-  <div class="app-control-error" *ngIf="form.errors as e">
-    <span *ngIf="e.invalid">Неверные имя пользователя или пароль.</span>
-  </div>
-
-  <input uxgInput type="text" lg formControlName="user" placeholder="Почта"/>
-  <div class="app-control-error" *ngIf="form.get('user').errors as e">
-    <span *ngIf="e.required">Обязательное поле</span>
-    <span *ngIf="e.minlength as e">Минимальная длина {{e.requiredLength}}, а сейчас {{e.actualLength}}</span>
-    <span *ngIf="e.email">Неверная почта</span>
-  </div>
-
-  <input uxgInput type="text" lg formControlName="password" placeholder="Пароль"/>
-  <div class="app-control-error" *ngIf="form.get('password').errors as e">
-    <span *ngIf="e.required">Обязательное поле</span>
-    <span *ngIf="e.minlength as e">Минимальная длина {{e.requiredLength}}, а сейчас {{e.actualLength}}</span>
-    <span *ngIf="e.pattern">Заглавные и строчные буквы и цифры.</span>
-  </div>
-
-  <div class="app-row app-align-items-center">
-    <uxg-switcher label="Запомнить меня" labelAlign="right" formControlName="rememberMe"></uxg-switcher>
-    <div class="app-btn-group">
-      <button uxgButton outline lg icon type="button" (click)="form.reset()">
-        <clr-icon shape="refresh" flip="horizontal" class="is-solid"></clr-icon>
-      </button>
-      <button uxgButton primary lg iconText [disabled]="form.invalid">
-        <clr-icon shape="login" class="is-solid"></clr-icon>
-        <span>Войти</span>
-      </button>
+    <!-- Whole form errors -->
+    <div class="app-control-error" *ngIf="form.errors as e">
+      <span *ngIf="e.invalid">Неверные имя пользователя или пароль.</span>
     </div>
-  </div>
-</form>`;
+
+    <!-- username input -->
+    <div class="app-control-wrap">
+      <input #user uxgInput lg type="text" formControlName="user"/>
+      <label class="app-control-label" (click)="user.focus()">Почта</label>
+
+      <!-- username errors -->
+      <div class="app-control-error" *ngIf="form.get('user').errors as e">
+        <span *ngIf="e.required">Обязательное поле</span>
+        <span *ngIf="e.email">Неверная почта</span>
+      </div>
+    </div>
+
+    <!-- password input -->
+    <div class="app-control-wrap">
+      <input uxgInput type="password" lg formControlName="password" placeholder="Пароль"/>
+
+      <!-- password errors -->
+      <ng-container *ngIf="form.get('password').errors as e">
+
+        <uxg-popover #errorPopover class="app-control-icon-error" *ngIf="errorsCount(e) > 1; else passowrdSingleError">
+          <clr-icon shape="app-info" size="16" (mouseenter)="errorPopover.show()" (mouseleave)="errorPopover.hide()"></clr-icon>
+          <div *uxgPopoverContent="popoverDir.topRight" class="app-primary-color">
+            <ng-container *ngTemplateOutlet="passwordErrors"></ng-container>
+          </div>
+        </uxg-popover>
+
+        <ng-template #passowrdSingleError>
+          <div class="app-control-error">
+            <ng-container *ngTemplateOutlet="passwordErrors"></ng-container>
+          </div>
+        </ng-template>
+
+        <ng-template #passwordErrors>
+          <div *ngIf="e.required">Обязательное поле</div>
+          <div *ngIf="e.minlength as e">Минимальная длина {{e.requiredLength}}, а сейчас {{e.actualLength}}</div>
+          <div *ngIf="e.pattern">Заглавные и строчные буквы и цифры.</div>
+        </ng-template>
+      </ng-container>
+    </div>
+
+    <!-- footer -->
+    <div class="app-row app-align-items-center">
+      <uxg-switcher label="Запомнить меня" labelAlign="right" formControlName="rememberMe"></uxg-switcher>
+      <div class="app-col"></div>
+      <div class="app-btn-group">
+        <button uxgButton outline lg icon type="button" (click)="form.reset()">
+          <clr-icon shape="refresh" flip="horizontal" class="is-solid"></clr-icon>
+        </button>
+        <button uxgButton primary lg iconText [disabled]="form.invalid">
+          <clr-icon shape="login" class="is-solid"></clr-icon>
+          <span>Войти</span>
+        </button>
+      </div>
+    </div>
+  </form>`;
 
   form = new FormGroup({
     user: new FormControl('', [Validators.required, Validators.email]),
@@ -48,6 +78,8 @@ export class UxgExampleInputComponent {
     ]),
     rememberMe: new FormControl()
   });
+
+  popoverDir = UxgPopoverContentDirection;
 
   errorsCount(errors: ValidationErrors): number {
     return Object.keys(errors).length;
