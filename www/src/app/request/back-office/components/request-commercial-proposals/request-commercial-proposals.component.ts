64657713b39074f2045367695a4fbbb116438eeb
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-01-17 14:48:25 +0300

    gpn_market-1057 Отображение списка кп

--- www/src/app/request/back-office/components/request-commercial-proposals/request-commercial-proposals.component.ts
+++ www/src/app/request/back-office/components/request-commercial-proposals/request-commercial-proposals.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({ templateUrl: './request-commercial-proposals.component.html' })
+@Component({templateUrl: './request-commercial-proposals.component.html'})
 export class RequestCommercialProposalsComponent implements OnInit {
 
   constructor() { }
--- www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
+++ www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestCommercialProposalsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  requestId: Uuid;
+
+  @Input() requestPositions: RequestPosition[] = [];
+  @Input() suppliers: ContragentList[];
+  @Output() sentForAgreement = new EventEmitter<{requestId: Uuid, selectedPositions: RequestPosition[]}>();
+
+  supplier: ContragentList;
+
+  get formPositions() {
+    return this.form.get('positions') as FormArray;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.requestId = this.route.snapshot.paramMap.get('id');
+
+    this.form = this.fb.group({
+      checked: false,
+      positions: this.fb.array([], CustomValidators.oneOrMoreSelected)
+    });
+
+    this.requestPositions.map(position => this.formPositions.push(
+      this.createFormGroupPosition(position)));
+  }
+
+  createFormGroupPosition(position: RequestPositionList) {
+    return this.fb.group({
+      checked: false,
+      position: position
+    });
   }
 
 }
