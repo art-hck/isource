284848e7517a66773646969e071641eb29429dd7
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-01-23 02:14:00 +0300

    gpn_market-1078 Доработка интерфейса списка КП

--- www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
+++ www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Inject, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {TechnicalProposal} from "../../models/technical-proposal";
 import {RequestPosition} from "../../models/request-position";
 import {RequestOfferPosition} from "../../models/request-offer-position";
@@ -10,6 +10,9 @@ import {AbstractControl, FormArray, FormBuilder, FormGroup} from "@angular/forms
 import {CustomValidators} from "../../../../shared/forms/custom.validators";
 import {RequestPositionList} from "../../models/request-position-list";
 import {ActivatedRoute, Route} from "@angular/router";
+import {ClrLoadingState} from "@clr/angular";
+import {GpnmarketConfigInterface} from "../../../../core/config/gpnmarket-config.interface";
+import { APP_CONFIG } from '@stdlib-ng/core';
 
 @Component({
   selector: 'app-request-commercial-proposals',
@@ -20,11 +23,15 @@ export class RequestCommercialProposalsComponent implements OnInit {
 
   form: FormGroup;
   requestId: Uuid;
+  appConfig: GpnmarketConfigInterface;
+
+  isFolded = [];
 
   @Input() requestPositions: RequestPosition[] = [];
   @Input() suppliers: ContragentList[];
   @Output() sentForAgreement = new EventEmitter<{ requestId: Uuid, selectedPositions: RequestPosition[] }>();
   @Output() addOffer = new EventEmitter<RequestPosition>();
+  @Output() cancelOffer = new EventEmitter<RequestPosition>();
 
   supplier: ContragentList;
 
@@ -32,9 +39,16 @@ export class RequestCommercialProposalsComponent implements OnInit {
     return this.form.get('positions') as FormArray;
   }
 
+  /**
+   * Время в течение которого бэкофис может отозвать КП (в секундах)
+   */
+  protected durationCancelPublish = 10 * 60;
+
   constructor(
     private fb: FormBuilder,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+  @Inject(APP_CONFIG) appConfig: GpnmarketConfigInterface) {
+    this.appConfig = appConfig;
   }
 
   ngOnInit() {
@@ -98,5 +112,37 @@ export class RequestCommercialProposalsComponent implements OnInit {
 
   newCommercialProposal(position) {
     this.addOffer.emit(position);
+  }
+
+  availableCancelPublishOffers(requestPosition: RequestPosition) {
+    return requestPosition.status === RequestPositionWorkflowSteps.RESULTS_AGREEMENT
+      && this.getDurationChangeStatus(requestPosition) < this.durationCancelPublish;
+  }
+
+  positionHasFinishedProcedure(requestPosition: RequestPosition): boolean {
+    return requestPosition.procedureEndDate && !requestPosition.hasProcedure;
+  }
+
+  getProcedureLink(requestPosition: RequestPosition): string {
+    const procedureUrl = this.appConfig.procedure.url;
+    const id = requestPosition.procedureId;
+
+    return procedureUrl + id;
+  }
+
+  /**
+   * Возвращает время в секундах, которое прошло с момента смены статуса ТП
+   * @param requestPosition
+   */
+  protected getDurationChangeStatus(requestPosition: RequestPosition): number {
+    return moment().diff(moment(requestPosition.statusChangedDate), 'seconds');
+  }
+
+  onCancelPublishOffers(requestPosition: RequestPosition) {
+    this.cancelOffer.emit(requestPosition);
+  }
+
+  isOverflow(positionCard: HTMLElement) {
+    return positionCard.scrollHeight > positionCard.clientHeight + 30;
   }
 }
