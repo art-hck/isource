dfe65d9ba6d9688fabd0fc640976639e4a4e2f3c
a.baydin <a.baydin@etpgpb.ru>
2020-01-27 16:34:35 +0300

    gpn_market-1111 Изменить процесс присоединения юзера к контрагенту

--- www/src/app/shared/directives/drag-file-upload.directive.ts
+++ www/src/app/shared/directives/drag-file-upload.directive.ts
@@ -10,21 +10,21 @@ export class DragFileUploadDirective {
   constructor(private el: ElementRef) {}
 
   @HostListener("dragover", ["$event"])
-  public onDragOver(e: DragEvent) {
+  public onDragOver(e: Event) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.add(this.hoverClass);
   }
 
   @HostListener("dragleave", ["$event"])
-  public onDragLeave(e: DragEvent) {
+  public onDragLeave(e: Event) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.remove(this.hoverClass);
   }
 
   @HostListener('drop', ['$event'])
-  public onDrop(e: DragEvent) {
+  public onDrop(e: Event | any) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.remove(this.hoverClass);
