5bc140a0b8033ad69570233bdcc7bae01e7d93f4
a.baydin <a.baydin@etpgpb.ru>
2020-01-28 13:42:36 +0300

    gpn_market-1098 доработки обработки доступа к страницам

--- www/src/app/core/can-activate-feature.guard.ts
+++ www/src/app/core/can-activate-feature.guard.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { FeatureService } from "./services/feature.service";
 import { UserInfoService } from "../user/service/user-info.service";
+import { AuthService } from "../auth/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateFeatureGuard implements CanActivate {
 
-  constructor(private router: Router, private feature: FeatureService, private userInfo: UserInfoService) {}
+  constructor(private router: Router, private feature: FeatureService, private userInfo: UserInfoService, private authService: AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
     if (route.data["feature"]) {
       if (this.feature.disabled(route.data["feature"])) {
-        this.router.navigate(['404']);
+        this.router.navigate(['/not-found']);
         return false;
       }
 
       if (!this.feature.allowed(route.data["feature"], this.userInfo.roles)) {
-        this.router.navigate(['/forbidden']);
+        if (this.userInfo.getUserInfo()) {
+          this.router.navigate(['/forbidden']);
+        } else {
+          const queryParams = ['/', '/auth/login'].indexOf(window.location.pathname) < 0 ? { returnUrl: window.location.pathname } : {};
+          this.router.navigate(['/auth/login'], { queryParams });
+        }
+
         return false;
       }
     }
