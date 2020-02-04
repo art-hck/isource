a910c337529193a9f6b0fd29e3d2c6f96012c3f5
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-02-04 15:44:30 +0300

    gpn_market-1078 Доработки по КП

--- www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
+++ www/src/app/request/common/components/request-commercial-proposals/request-commercial-proposals.component.ts
@@ -151,26 +151,4 @@ export class RequestCommercialProposalsComponent implements OnInit {
   isOverflow(positionCard: HTMLElement) {
     return positionCard.scrollHeight > positionCard.clientHeight + 30;
   }
-
-  /**
-   * Функция возвращает надпись с количеством документов
-   *
-   * @param count
-   */
-  getDocumentCountLabel(count: number): string {
-    const cases = [2, 0, 1, 1, 1, 2];
-    const strings = ['документ', 'документа', 'документов'];
-
-    const documentsString = strings[
-      ( count % 100 > 4 && count % 100 < 20 ) ?
-        2 :
-        cases[
-          (count % 10 < 5) ?
-            count % 10 :
-            5
-          ]
-      ];
-
-    return count + ' ' + documentsString;
-  }
 }
