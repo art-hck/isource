4e303d1da0163a326f12b5ea9e32c09066ea3975
Karen <id12102180@ya.ru>
2020-02-06 16:47:39 +0300

    gpn_market-1172 Добавил переход к позиции из списка позиций сотрудника

--- www/src/app/employees/components/user-card/user-card-request-list/user-card-request-list.component.ts
+++ www/src/app/employees/components/user-card/user-card-request-list/user-card-request-list.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { UserInfoRequestItem } from "../../../models/user-info";
 import { Router } from "@angular/router";
-import { RequestsList } from "../../../../request/common/models/requests-list/requests-list";
 
 @Component({
   selector: 'app-user-card-request-list',
   templateUrl: './user-card-request-list.component.html',
   styleUrls: ['./user-card-request-list.component.scss']
 })
-export class UserCardRequestListComponent implements OnInit {
+export class UserCardRequestListComponent {
 
   @Input() requests: UserInfoRequestItem[];
 
@@ -16,15 +15,12 @@ export class UserCardRequestListComponent implements OnInit {
     protected router: Router,
   ) { }
 
-  ngOnInit() {
-  }
-
   calcPieChart(request: any): number {
     const completedItems =  request.completedCount / request.positionCount * 100;
     return (65 - (65 * completedItems) / 100);
   }
 
-  onRowClick(request: RequestsList): void {
+  onRowClick(request: UserInfoRequestItem): void {
     this.router.navigateByUrl(`/requests/backoffice/${request.request.id}`);
   }
 
--- www/src/app/request/common/models/user-list-request-position.ts
+++ www/src/app/request/common/models/user-list-request-position.ts
@@ -1,5 +1,7 @@
 import { RequestPosition } from "./request-position";
+import { Uuid } from "../../../cart/models/uuid";
 
 export class UserListRequestPosition extends RequestPosition {
   requestNumber: number;
+  requestId: Uuid;
 }
