7bba3daa650fa33df61489ec92853caa08af81c1
Karen <id12102180@ya.ru>
2020-02-10 16:48:37 +0300

    gpn_market-1062 Фикс ошибки

--- www/src/app/request/common/components/request-position-form/request-position-form.component.ts
+++ www/src/app/request/common/components/request-position-form/request-position-form.component.ts
@@ -121,15 +121,17 @@ export class RequestPositionFormComponent implements OnInit {
   submit() {
     let submit$: Observable<RequestPosition>;
 
-    // Проверяем, есть ли правки для сохранения или данные в форме остались без изменений
-    const positionInfoNotChanged = Object.entries(this.form.value).every(([key, value]) => {
-      return this.position[key].toString() === value.toString();
-    });
+    if (this.position.id) {
+      // Проверяем, есть ли правки для сохранения или данные в форме остались без изменений
+      const positionInfoNotChanged = Object.entries(this.form.value).every(([key, value]) => {
+        return this.position[key].toString() === value.toString();
+      });
 
-    // Если изменений нет, эмитим событие для закрытия окна и прерываем сабмит
-    if (positionInfoNotChanged) {
-      this.cancel.emit();
-      return;
+      // Если изменений нет, эмитим событие для закрытия окна и прерываем сабмит
+      if (positionInfoNotChanged) {
+        this.cancel.emit();
+        return;
+      }
     }
 
     if (this.position.id) {
