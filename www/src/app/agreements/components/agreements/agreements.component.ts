8ad45d985724fdbdea87bd15dc075d4df44da2c3
a.baydin <a.baydin@etpgpb.ru>
2020-02-12 17:25:31 +0300

    gpn_market-1203 фикс когда нет соответствующего обработчика события на статус

--- www/src/app/agreements/components/agreements/agreements.component.ts
+++ www/src/app/agreements/components/agreements/agreements.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Agreements } from "../../../dashboard/models/Agreements";
 import { RequestPositionWorkflowSteps as PositionStatus } from "../../../request/common/enum/request-position-workflow-steps";
+import { RequestPosition } from "../../../request/common/models/request-position";
 
 @Component({
   selector: 'app-agreements',
@@ -26,5 +27,18 @@ export class AgreementsComponent {
       label: "Рассмотреть КП",
       path: "commercial-proposals"
     },
+
+    [PositionStatus.ON_CUSTOMER_APPROVAL]: {
+      label: "Рассмотреть позицию"
+    },
   };
+
+  getRouterLink(position: RequestPosition): any[] {
+    const routerLink = ['/requests/customer', position.request.id];
+
+    if (this.labels[position.status] && this.labels[position.status].path) {
+      routerLink.push(this.labels[position.status].path);
+    }
+    return routerLink;
+  }
 }
