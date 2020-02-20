7053449160ed8d0deb6fb87372a1cef18644eb2d
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-02-20 11:15:32 +0300

    gpn_market-1233 Обновить е2е тесты

--- www/e2e/src/tests/login/login.e2e-spec.ts
+++ www/e2e/src/tests/login/login.e2e-spec.ts
@@ -4,27 +4,28 @@ import { Login, Role } from "./login";
 
 const loginData = require("../../test-data/login-data");
 
-fdescribe('Логин', async () => {
+describe('Логин', () => {
 
   const EC = protractor.ExpectedConditions;
   const login = new Login();
 
-  beforeEach(async () => {
-    await browser.get(browser.baseUrl);
+  beforeEach(() => {
+    browser.get(browser.baseUrl);
   });
 
-  it('Отображение страница логина', async () => {
+  it('Отображение страница логина', () => {
     expect(LoginPage.welcomeMessage().getText()).toEqual('Добро пожаловать');
     expect(LoginPage.reqLink().getText()).toEqual(('Регистрация'));
     expect(LoginPage.forgotPasswordLink().getText()).toEqual('Забыли пароль?');
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/auth/login');
   });
 
-  fit('Успешный логин бэкофисом', async () => {
+  it('Успешный логин бэкофисом', () => {
 
     login.login(Role.BACKOFFICE);
-    //await browser.wait(EC.visibilityOf(element(by.tagName('h1'))));
-    expect(await browser.getCurrentUrl()).toEqual(browser.baseUrl + '/requests/backoffice');
+    browser.wait(EC.visibilityOf(element(by.tagName('h1'))), 5000);
+
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/requests/backoffice');
     expect(LoginPage.menuItems().getText()).toEqual(loginData.backoffice.menuItems);
     expect(LoginPage.usernameButton().getText()).toEqual(browser.params.login.backoffice.user);
   });
@@ -34,16 +35,16 @@ fdescribe('Логин', async () => {
     expect(LoginPage.welcomeMessage().getText()).toEqual('Добро пожаловать');
   });
 
-  it('Успешный логин заказчиком', () => {
+  fit('Успешный логин заказчиком', () => {
     login.login(Role.CUSTOMER);
     browser.wait(EC.visibilityOf(LoginPage.requestListTitle()));
-    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/requests/customer');
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/dashboard');
     expect(LoginPage.menuItems().getText()).toEqual(loginData.customer.menuItems);
     expect(LoginPage.usernameButton().getText()).toEqual(browser.params.login.customer.user);
   });
 
-  it('Успешный выход заказчиком', () => {
+  fit('Успешный выход заказчиком', () => {
     login.logout();
-    expect(LoginPage.welcomeMessage().getText()).toEqual('Добро пожаловать в');
+    expect(LoginPage.welcomeMessage().getText()).toEqual('Добро пожаловать');
   });
 });
