ff9e4edd8ddc414815c92f6a02404b210e5477b6
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-03-25 10:35:22 +0300

    gpn_market-1429 Пропала кнопка "Отчет Прима-ИНформ"

--- www/src/app/contragent/components/contragent-info/contragent-info.component.ts
+++ www/src/app/contragent/components/contragent-info/contragent-info.component.ts
@@ -10,7 +10,17 @@ import { ContragentService } from "../../services/contragent.service";
 export class ContragentInfoComponent {
 
   @Input() contragent: ContragentInfo;
+  @Input() modalView = true;
 
   constructor(
+    private getContragentService: ContragentService
   ) { }
+
+  onDownloadPrimaInformReport(): void {
+    this.getContragentService.downloadPrimaInformReport(this.contragent.id);
+  }
+
+  getLoaderState() {
+    return this.getContragentService.loading;
+  }
 }
