134c36092296634d9e7ffb70b8e7e3efc0069397
a.baydin <a.baydin@etpgpb.ru>
2020-03-25 11:26:14 +0300

    gpn_market-1437 pipeline

--- www/src/app/request/common/dictionaries/payment-terms-labels.ts
+++ www/src/app/request/common/dictionaries/payment-terms-labels.ts
@@ -4,4 +4,4 @@ export const PaymentTermsLabels: Record<PaymentTerms, string> = {
   [PaymentTerms.Days30] : "30 банковских дней по факту поставки",
   [PaymentTerms.Days60] : "60 банковских дней по факту поставки",
   [PaymentTerms.Days90] : "90 банковских дней по факту поставки"
-}
+};
