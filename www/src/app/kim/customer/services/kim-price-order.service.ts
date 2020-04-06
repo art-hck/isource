45e056465b9e9983dab87823b4105459e83d3191
a.baydin <a.baydin@etpgpb.ru>
2020-04-06 16:21:06 +0300

    gpn_market-1500 Интеграция списка ЦЗ

--- www/src/app/kim/customer/services/kim-price-order.service.ts
+++ www/src/app/kim/customer/services/kim-price-order.service.ts
@@ -5,15 +5,13 @@ import { KimPriceOrder } from "../../common/models/kim-price-order";
 import { KimPriceOrdersMock } from "./kim-price-orders.mock";
 import { of } from "rxjs";
 import { delay } from "rxjs/operators";
-import { Guid } from "guid-typescript";
 
 @Injectable()
 export class KimPriceOrderService {
   constructor(private api: HttpClient, private formDataService: FormDataService) {}
 
   list() {
-    const url = `kim/customer/add-price-order/list`;
-    return of(KimPriceOrdersMock).pipe(delay(1000));
+    const url = `kim/customer/price-order/list`;
     return this.api.get<KimPriceOrder[]>(url);
   }
 
