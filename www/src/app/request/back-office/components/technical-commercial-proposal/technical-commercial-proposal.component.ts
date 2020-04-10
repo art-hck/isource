60dd9afc08577a982d6cbbd03c3cd30ec6a367fc
a.baydin <a.baydin@etpgpb.ru>
2020-04-10 12:29:08 +0300

    gpn_market-1534 Отображать кнопку отправки черновика ТКП на согласование в режиме просмотра

--- www/src/app/request/back-office/components/technical-commercial-proposal/technical-commercial-proposal.component.ts
+++ www/src/app/request/back-office/components/technical-commercial-proposal/technical-commercial-proposal.component.ts
@@ -2,6 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TechnicalCommercialProposal } from "../../../common/models/technical-commercial-proposal";
 import { getCurrencySymbol } from "@angular/common";
 import { Request } from "../../../common/models/request";
+import { Select, Store } from "@ngxs/store";
+import { TechnicalCommercialProposals } from "../../actions/technical-commercial-proposal.actions";
+import { TechnicalCommercialProposalState } from "../../states/technical-commercial-proposal.state";
+import { Observable } from "rxjs";
+import { StateStatus } from "../../../common/models/state-status";
+import Publish = TechnicalCommercialProposals.Publish;
 
 @Component({
   selector: 'app-request-technical-commercial-proposal',
@@ -12,7 +18,14 @@ import { Request } from "../../../common/models/request";
 export class TechnicalCommercialProposalComponent {
   @Input() request: Request;
   @Input() technicalCommercialProposal: TechnicalCommercialProposal;
+  @Select(TechnicalCommercialProposalState.status) status$: Observable<StateStatus>;
   state: "view" | "edit" = "view";
   folded = false;
   getCurrencySymbol = getCurrencySymbol;
+
+  constructor(private store: Store) {}
+
+  publish() {
+    return this.store.dispatch(new Publish(this.technicalCommercialProposal));
+  }
 }
