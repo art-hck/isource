3c9b42ed8a01868555d55a2859813b43af5bedb0
a.baydin <a.baydin@etpgpb.ru>
2020-04-13 01:39:43 +0300

    gpn_market-1559 Remove order module

--- www/src/app/order/dictinaries/position-supplier-response-type-names.ts
+++
@@ -1,6 +0,0 @@
-import { PositionSupplierResponseTypes } from '../enums/position-supplier-response-types';
-
-export const PositionSupplierResponseTypeNames: {[key: string]: string} = {};
-
-PositionSupplierResponseTypeNames[PositionSupplierResponseTypes.IN_STOCK] = 'В наличии';
-PositionSupplierResponseTypeNames[PositionSupplierResponseTypes.OUT_OF_STOCK] = 'Отсутствует';
--- www/src/app/order/models/page.ts
+++
@@ -1,9 +0,0 @@
-export class Page<T> {
-  totalCount: number;
-  entities: Array<T>;
-
-  constructor(entities: Array<T>, totalCount: number) {
-    this.totalCount = totalCount;
-    this.entities = entities;
-  }
-}
