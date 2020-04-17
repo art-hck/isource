76f5c359b38b894c84e6fd7800a0e681d6267c76
Исраелян Карен <id12102180@ya.ru>
2020-04-17 14:55:54 +0300

    gpn_market-1433 Добавление и редактирование сотрудника

--- www/src/app/employee/components/employee-list/employee-list.component.ts
+++ www/src/app/employee/components/employee-list/employee-list.component.ts
@@ -1,10 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { EmployeeItem } from "../../models/employee-item";
-import { EmployeeService } from "../../services/employee.service";
 import { UserInfoService } from "../../../user/service/user-info.service";
 import { Router } from "@angular/router";
-import { EmployeeInfoBrief } from "../../models/employee-info";
-import { Uuid } from "../../../cart/models/uuid";
 
 @Component({
   selector: 'app-employee-list',
