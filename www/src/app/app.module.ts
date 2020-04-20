362898832c0227b3f5855896cb90ba11e73ccfc5
a.baydin <a.baydin@etpgpb.ru>
2020-04-20 03:45:30 +0300

    gpn_market-1611 Оптимизация проекта

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { LOCALE_ID, NgModule } from '@angular/core';
 import localeRu from '@angular/common/locales/ru';
-import { registerLocaleData } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from "./core/core.module";
-import { PagesModule } from "./pages/pages.module";
-import { AppConfig } from './config/app.config';
-import { HttpClientModule } from "@angular/common/http";
-import { WebsocketModule } from "./websocket/websocket.module";
-import { UxgIcons, UxgModule } from "uxg";
-import { UserModule } from "./user/user.module";
+import { BrowserModule } from '@angular/platform-browser';
 import { ClarityIcons } from "@clr/icons";
-import { AgreementsModule } from "./agreements/agreements.module";
+import { HttpClientModule } from "@angular/common/http";
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { NgxsModule } from "@ngxs/store";
 import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { registerLocaleData } from '@angular/common';
+import { UxgIcons, UxgModule } from "uxg";
+
 import { APP_CONFIG } from "./core/config/gpnmarket-config.interface";
+import { AppComponent } from './app.component';
+import { AppConfig } from './config/app.config';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from "./core/core.module";
 import { ToastListModule } from "./shared/components/toast-list/toast-list.module";
+import { WebsocketModule } from "./websocket/websocket.module";
 
 registerLocaleData(localeRu, 'ru');
 ClarityIcons.add(UxgIcons);
@@ -26,22 +23,16 @@ ClarityIcons.add(UxgIcons);
 @NgModule({
   declarations: [ AppComponent ],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
+    BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-
-    AgreementsModule,
-    PagesModule,
     CoreModule,
-    UserModule,
-    ToastListModule,
-    UxgModule,
     NgxsModule.forRoot(),
     NgxsReduxDevtoolsPluginModule.forRoot(),
-    WebsocketModule.config({
-      url: AppConfig.endpoints.ws
-    })
+    ToastListModule,
+    UxgModule,
+    WebsocketModule.config({ url: AppConfig.endpoints.ws })
   ],
   providers: [
     { provide: APP_CONFIG, useValue: AppConfig },
