23c55b3017f0b5a076c03835a1aa4b1b547606c7
a.baydin <a.baydin@etpgpb.ru>
2020-04-27 11:16:58 +0300

    gpn_market-1657 Правки по ревью

--- www/projects/uxg/src/modules/modal/uxg-modal.component.ts
+++ www/projects/uxg/src/modules/modal/uxg-modal.component.ts
@@ -4,7 +4,6 @@ import { UxgModalFooterDirective } from "./uxg-modal-footer.directive";
 @Component({
   selector: 'uxg-modal',
   templateUrl: './uxg-modal.component.html',
-  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UxgModalComponent {
   @ContentChild(UxgModalFooterDirective, { read: TemplateRef }) footerTpl: TemplateRef<ElementRef>;
@@ -23,13 +22,11 @@ export class UxgModalComponent {
   open() {
     this.state = true;
     this.stateChange.emit(true);
-    // this.cd.detectChanges();
   }
 
   @HostListener('document:keyup.esc')
   close() {
     this.state = false;
     this.stateChange.emit(false);
-    // this.cd.detectChanges();
   }
 }
