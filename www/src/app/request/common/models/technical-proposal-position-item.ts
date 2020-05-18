dc8692aea4a9cb284feacd4a25d2116ace2aec7c
Исраелян Карен <id12102180@ya.ru>
2020-05-18 16:09:21 +0300

    gpn_market-1762 Правки по ревью

--- www/src/app/request/common/models/technical-proposal-position-item.ts
+++
@@ -1,6 +0,0 @@
-import { TechnicalProposalPosition } from "./technical-proposal-position";
-
-export class TechnicalProposalPositionItem {
-  proposal: TechnicalProposalPosition;
-  checked?: boolean;
-}
