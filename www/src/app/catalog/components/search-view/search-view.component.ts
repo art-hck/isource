f0087a9ac4576a99308c34b1f7ae19a85b70642b
a.baydin <a.baydin@etpgpb.ru>
2020-05-19 11:30:07 +0300

    gpn_market-1805 Добавить кнопку загрузки каталога из интерфейса бэкофиса

--- www/src/app/catalog/components/search-view/search-view.component.ts
+++ www/src/app/catalog/components/search-view/search-view.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CatalogService } from "../../services/catalog.service";
 import { CatalogPosition } from "../../models/catalog-position";
 import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { CatalogCategoryFilter } from "../../models/catalog-category-filter";
+import { FeatureService } from "../../../core/services/feature.service";
+import { takeUntil } from "rxjs/operators";
+import { UxgModalComponent } from "uxg";
 
 @Component({
   selector: 'app-catalog-search-view',
   templateUrl: './search-view.component.html',
   styleUrls: ['./search-view.component.scss']
 })
-export class SearchViewComponent implements OnInit {
+export class SearchViewComponent implements OnInit, OnDestroy {
+  @ViewChild('uploadTemplateModal') uploadTemplateModal: UxgModalComponent;
   positions$: Observable<CatalogPosition[]>;
   query: string;
+  destroy$ = new Subject();
+  public templateFiles: File[] = [];
+  public templateLoading: boolean;
 
-  constructor(private catalogService: CatalogService, private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    public featureService: FeatureService,
+    public catalogService: CatalogService
+  ) {
   }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(routeParams => {
+    this.route.queryParams.pipe(takeUntil(this.destroy$)).subscribe(routeParams => {
       this.query = routeParams.q;
       this.positions$ = this.catalogService.searchPositionsByName(this.query);
     });
@@ -26,5 +37,21 @@ export class SearchViewComponent implements OnInit {
 
   filter(filter: CatalogCategoryFilter) {
     this.positions$ = this.catalogService.searchPositionsByName(this.query, filter);
+  }
+
+  uploadTemplate() {
+    this.templateLoading = true;
+    this.catalogService.uploadTemplate(this.templateFiles)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.uploadTemplateModal.close();
+        this.templateLoading = false;
+        this.positions$ = this.catalogService.searchPositionsByName(this.query);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
