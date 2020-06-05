3ec365ee9d75135d806b52cb9fac4633b494031e
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-06-05 09:20:33 +0300

    gpn_market-1924 правки по ревью

--- www/src/app/agreements/common/components/agreements/agreements.component.ts
+++ www/src/app/agreements/common/components/agreements/agreements.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Agreement } from "../../models/Agreement";
 import { AgreementActionLink } from "../../../back-office/dictionaries/agreement-action-link";
+import { UserInfoService } from "../../../../user/service/user-info.service";
 
 @Component({
   selector: 'app-agreements',
@@ -10,8 +11,10 @@ import { AgreementActionLink } from "../../../back-office/dictionaries/agreement
 export class AgreementsComponent {
   @Input() agreements: Agreement[];
 
+  constructor(public userInfoService: UserInfoService) {}
+
   getRouterLink(agreement: Agreement): any[] {
-    const routerLink = ['/requests/backoffice/', agreement.request.id];
+    const routerLink = [this.userInfoService.isCustomer() ? '/requests/customer/' : '/requests/backoffice/', agreement.request.id];
     if (AgreementActionLink[agreement.action.name]) {
       routerLink.push(AgreementActionLink[agreement.action.name]);
     }
