5f60b1cb5f084d33569d1acef32eae2518c7aa39
Исраелян Карен <id12102180@ya.ru>
2020-07-21 00:43:05 +0300

    gpn_market-2187 Добавил фильтр в раздел Согласований для заказчика

--- www/src/app/agreements/common/agreements-common.module.ts
+++ www/src/app/agreements/common/agreements-common.module.ts
@@ -3,16 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { SharedModule } from "../../shared/shared.module";
 import { AgreementsComponent } from './components/agreements/agreements.component';
+import { AgreementsListFilterComponent } from "./components/agreements/agreements-list-filter/agreements-list-filter.component";
+import { FilterSectionComponent } from "./components/agreements/agreements-list-filter/filter-section/filter-section.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
-  declarations: [AgreementsComponent],
+  declarations: [
+    AgreementsComponent,
+    AgreementsListFilterComponent,
+    FilterSectionComponent
+  ],
   imports: [
     RouterModule,
     SharedModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
-  exports: [AgreementsComponent]
+  exports: [
+    AgreementsComponent,
+    AgreementsListFilterComponent
+  ]
 })
 export class AgreementsCommonModule {
 }
--- www/src/app/agreements/customer/components/agreements/agreements.component.ts
+++ www/src/app/agreements/customer/components/agreements/agreements.component.ts
@@ -1,16 +1,22 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
 import { Select, Store } from "@ngxs/store";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { StateStatus } from "../../../../request/common/models/state-status";
 import { AgreementListState } from "../../states/agreement-list.state";
 import { AgreementListActions } from "../../actions/agreement-list.actions";
 import Fetch = AgreementListActions.Fetch;
-import { map } from "rxjs/operators";
+import { map, scan, takeUntil, tap, throttleTime } from "rxjs/operators";
 import { ActivatedRoute, Router } from "@angular/router";
 import { FormControl, FormGroup } from "@angular/forms";
 import { Agreement } from "../../../common/models/Agreement";
 import { AgreementAction } from "../../../back-office/enum/agreement-action";
 import { AgreementActionFilters } from "../../dictionaries/agreement-action-label";
+import { AvailableFilters } from "../../../../request/back-office/models/available-filters";
+import { AgreementsListFilter } from "../../../common/models/agreements-list/agreements-list-filter";
+import { RequestsListSort } from "../../../../request/common/models/requests-list/requests-list-sort";
+import { RequestsListFilter } from "../../../../request/common/models/requests-list/requests-list-filter";
+import { RequestStatus } from "../../../../request/common/enum/request-status";
+import moment from "moment";
 
 @Component({
   templateUrl: './agreements.component.html',
@@ -21,6 +27,9 @@ export class AgreementsComponent implements OnInit {
   @Select(AgreementListState.status) status$: Observable<StateStatus>;
   @Select(AgreementListState.totalCount) totalCount$: Observable<number>;
 
+  filters: AgreementsListFilter = {};
+  filterOpened = false;
+
   readonly pageSize = 10;
   readonly actions: {type: AgreementAction[], label: string}[] = AgreementActionFilters;
   pages$: Observable<number>;
@@ -28,6 +37,13 @@ export class AgreementsComponent implements OnInit {
   form = new FormGroup({
     actions: new FormControl(null)
   });
+
+  get activeFilters() {
+    return this.filters && Object.entries(this.filters)
+      .filter(([k, v]: [keyof AgreementsListFilter, any]) => {
+        return k !== 'actions' && v instanceof Array && v.length > 0 || !(v instanceof Array) && v;
+      });
+  }
 
   constructor(
     public store: Store,
@@ -40,13 +56,24 @@ export class AgreementsComponent implements OnInit {
     this.form.valueChanges.subscribe(
       () => {
         this.router.navigate(["."], {relativeTo: this.route, queryParams: null});
-        const action = this.form.value;
-        this.store.dispatch(new Fetch(action, 0, this.pageSize));
+        this.filters.actions = this.form.value.actions;
+        this.store.dispatch(new Fetch(this.filters, 0, this.pageSize));
       }
     );
   }
 
   loadPage(page: number) {
-    this.store.dispatch(new Fetch(this.form.value, (page - 1) * this.pageSize, this.pageSize));
+    this.store.dispatch(new Fetch(this.filters, (page - 1) * this.pageSize, this.pageSize));
+  }
+
+  filterList(filters: AgreementsListFilter) {
+    this.filters = filters;
+
+    this.filters.actions = (this.form.get('actions').value?.length > 0) ? this.form.get('actions').value : null;
+    this.filters.issuedDateFrom = filters.issuedDateFrom !== '' ? moment(moment(filters.issuedDateFrom, 'DD.MM.YYYY')).format('YYYY-MM-DD') : null;
+    this.filters.issuedDateTo = filters.issuedDateTo !== '' ? moment(moment(filters.issuedDateTo, 'DD.MM.YYYY')).format('YYYY-MM-DD') : null;
+    this.filters.contragents = filters.contragents.length > 0 ? filters.contragents : null;
+
+    this.store.dispatch(new Fetch(this.filters, 0, this.pageSize));
   }
 }
