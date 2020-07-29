00d645c9355908a75bb577b0d3be7735f9649757
Исраелян Карен <id12102180@ya.ru>
2020-07-29 11:31:30 +0300

    gpn_market-2309 Добавил всплывающее сообщение о некорректном кол-ве позиций в корзине

--- www/src/app/shared/pipes/phone.pipe.ts
+++ www/src/app/shared/pipes/phone.pipe.ts
@@ -10,8 +10,8 @@ export class PhonePipe implements PipeTransform {
 
     return value ? '+' +
       // приводим к международному формату русские номера
-      (value[0] === '8' ? '7' : value[0]) + ' ('
-      + value.substr(1, 3) + ') '
+      (value[0] === '8' ? '7' : value[0]) + ' '
+      + value.substr(1, 3) + ' '
       + value.substr(4, 3) + '-'
       + value.substr(7, 2) + '-'
       + value.substr(9) : null;
