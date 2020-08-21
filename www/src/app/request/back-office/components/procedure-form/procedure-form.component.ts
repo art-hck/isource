da03bc082492db990bd12fa8313df959d716db57
Исраелян Карен <id12102180@ya.ru>
2020-08-21 13:14:15 +0300

    gpn_market-2499 Фикс значения в поле для причины отстутсвия НМЦ

--- www/src/app/request/back-office/components/procedure-form/procedure-form.component.ts
+++ www/src/app/request/back-office/components/procedure-form/procedure-form.component.ts
@@ -87,7 +87,7 @@ export class ProcedureFormComponent implements OnInit, OnDestroy {
         dateEndRegistration: [null, CustomValidators.currentOrFutureDate()],
         dateSummingUp: [null, [Validators.required, CustomValidators.currentOrFutureDate()]],
         withoutTotalPrice: [this.defaultProcedureValue("withoutTotalPrice", false)],
-        withoutTotalPriceReason: [this.defaultProcedureValue("withoutTotalPrice", 'НМЦ не рассчитывалась'), [Validators.required]],
+        withoutTotalPriceReason: [this.defaultProcedureValue("withoutTotalPriceReason", 'НМЦ не рассчитывалась'), [Validators.required]],
         dishonestSuppliersForbidden: this.defaultProcedureValue("dishonestSuppliersForbidden", false),
         publicAccess: [true, Validators.required],
         okpd2: ["", Validators.required],
