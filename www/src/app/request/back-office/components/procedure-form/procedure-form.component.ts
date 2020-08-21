90a2ec0b5a376c838f004c384961998b3fdca7d4
Исраелян Карен <id12102180@ya.ru>
2020-08-21 16:35:06 +0300

    gpn_market-2500 Учитывание времени при сравнении дат завершения приёма заявок и подведения итогов

--- www/src/app/request/back-office/components/procedure-form/procedure-form.component.ts
+++ www/src/app/request/back-office/components/procedure-form/procedure-form.component.ts
@@ -148,7 +148,7 @@ export class ProcedureFormComponent implements OnInit, OnDestroy {
         const dateSummingUp = this.form.get("general.dateSummingUp").value;
 
         if (this.form.get("general.dateSummingUp").value) {
-          if (moment(date, "DD.MM.YYYY").isAfter(moment(dateSummingUp, "DD.MM.YYYY"))) {
+          if (moment(date, "DD.MM.YYYY HH:mm").isAfter(moment(dateSummingUp, "DD.MM.YYYY HH:mm"))) {
             this.form.get("general.dateSummingUp").setErrors({ afterEndRegistrationDate: true});
           } else {
             this.form.get("general.dateSummingUp").setErrors(null);
@@ -163,7 +163,7 @@ export class ProcedureFormComponent implements OnInit, OnDestroy {
         const dateEndRegistration = this.form.get("general.dateEndRegistration").value;
 
         if (this.form.get("general.dateSummingUp").value) {
-          if (moment(date, "DD.MM.YYYY").isBefore(moment(dateEndRegistration, "DD.MM.YYYY"))) {
+          if (moment(date, "DD.MM.YYYY HH:mm").isBefore(moment(dateEndRegistration, "DD.MM.YYYY HH:mm"))) {
             this.form.get("general.dateSummingUp").setErrors({ afterEndRegistrationDate: true});
           } else {
             this.form.get("general.dateSummingUp").setErrors(null);
