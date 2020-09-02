b133fa3b1e0df4653eb3d4cb7e93405be9bf17a0
a.mikhaylova <a.mikhaylova@etpgpb.ru>
2020-09-02 16:00:20 +0300

    gpn_market-2549 правки по ревью

--- www/src/app/request/common/dictionaries/delivery-type-labels.ts
+++ www/src/app/request/common/dictionaries/delivery-type-labels.ts
@@ -1,6 +1,6 @@
 import { DeliveryType } from "../../back-office/enum/delivery-type";
 
-export const DeliveryTypeLabels = {
+export const DeliveryTypeLabels: Record<DeliveryType, string> = {
   [DeliveryType.INCLUDED]: "Доставка включена в стоимость позиций",
   [DeliveryType.NOT_INCLUDED]: "Доставка не включена в стоимость позиций",
   [DeliveryType.PICKUP]: "Самовывоз",
