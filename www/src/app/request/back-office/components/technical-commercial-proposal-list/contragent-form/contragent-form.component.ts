bf2f70354248c39db3ac9b1694efe15ee91ef368
Alisia Mikhaylova <a.mikhaylova@etpgpb.ru>
2020-09-07 19:02:20 +0300

    gpn_market-2597 Верстка и добавление контролов в форму создания контрагента матрицы ТКП

--- www/src/app/request/back-office/components/technical-commercial-proposal-list/contragent-form/contragent-form.component.ts
+++ www/src/app/request/back-office/components/technical-commercial-proposal-list/contragent-form/contragent-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, Validators } from "@angular/forms";
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ContragentList } from "../../../../../contragent/models/contragent-list";
 import { shareReplay } from "rxjs/operators";
 import { ContragentService } from "../../../../../contragent/services/contragent.service";
@@ -8,23 +8,57 @@ import { TechnicalCommercialProposals } from "../../../actions/technical-commerc
 import { Request } from "../../../../common/models/request";
 import CreateContragent = TechnicalCommercialProposals.CreateContragent;
 import { ContragentShortInfo } from "../../../../../contragent/models/contragent-short-info";
+import { PositionCurrency } from "../../../../common/enum/position-currency";
+import { DeliveryType } from "../../../enum/delivery-type";
+import { DeliveryTypeLabels } from "../../../../common/dictionaries/delivery-type-labels";
+import { CurrencyLabels } from "../../../../common/dictionaries/currency-labels";
+import { getCurrencySymbol } from "@angular/common";
 
 @Component({
   selector: 'technical-commercial-proposal-contragent-form',
-  templateUrl: 'contragent-form.component.html'
+  templateUrl: 'contragent-form.component.html',
+  styleUrls: ['./contragent-form.component.scss'],
 })
 export class TechnicalCommercialProposalContragentFormComponent {
   @Input() request: Request;
   @Input() selectedContragents: ContragentShortInfo[];
   @Output() close = new EventEmitter();
-  readonly form = this.fb.group({ supplier: [null, Validators.required] });
+  readonly deliveryType = DeliveryType;
+  readonly deliveryTypeLabel = DeliveryTypeLabels;
+  readonly currencies = Object.entries(CurrencyLabels);
+  readonly getCurrencySymbol = getCurrencySymbol;
   readonly contragents$ = this.contragentService.getContragentList().pipe(shareReplay(1));
+  form: FormGroup;
 
   constructor(
     private contragentService: ContragentService,
     private fb: FormBuilder,
     private store: Store,
+    private cd: ChangeDetectorRef
   ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      supplier: [null, Validators.required],
+      files: [[]],
+      deliveryType: [this.deliveryType.INCLUDED],
+      deliveryAdditionalTerms: ['', Validators.required],
+      warrantyConditions: ['', Validators.required],
+      deliveryPrice: [''],
+      deliveryCurrency: [PositionCurrency.RUB],
+      deliveryPickup: ['']
+    });
+
+    this.form.valueChanges.subscribe(() =>{
+      this.form.get('deliveryPickup').setValidators(
+        this.form.get('deliveryType').value === this.deliveryType.PICKUP ? [Validators.required] : null);
+
+      this.form.get('deliveryPrice').setValidators(
+        this.form.get('deliveryType').value === this.deliveryType.NOT_INCLUDED ? [Validators.required] : null);
+
+    });
+    this.cd.detectChanges();
+  }
 
   search(query: string, contragents: ContragentList[]) {
     return contragents.filter(c => c.shortName.toLowerCase().indexOf(query.toLowerCase()) >= 0 || c.inn.indexOf(query) >= 0);
