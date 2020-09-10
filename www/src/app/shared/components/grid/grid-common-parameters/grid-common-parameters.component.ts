188255b24e2fe3e051da8fbfcbc735d7b24940ce
Alisia Mikhaylova <a.mikhaylova@etpgpb.ru>
2020-09-10 09:59:30 +0300

    gpn_market-2598 Добавила в гит компонент общих параметров

---
+++ www/src/app/shared/components/grid/grid-common-parameters/grid-common-parameters.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TechnicalCommercialProposal } from "../../../../request/common/models/technical-commercial-proposal";
+import { ProposalHelperService } from "../proposal-helper.service";
+import { TechnicalCommercialProposalByPosition } from "../../../../request/common/models/technical-commercial-proposal-by-position";
+import { getCurrencySymbol } from "@angular/common";
+
+@Component({
+  selector: 'app-grid-common-parameters',
+  templateUrl: './grid-common-parameters.component.html',
+  styleUrls: ['./grid-common-parameters.component.scss']
+})
+export class GridCommonParametersComponent implements OnInit {
+  @Input() proposal: TechnicalCommercialProposal;
+  @Input() proposalsByPos: TechnicalCommercialProposalByPosition[];
+  getCurrencySymbol = getCurrencySymbol;
+
+  constructor(
+    public helper: ProposalHelperService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
