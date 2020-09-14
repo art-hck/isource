d19fc0b3c6097cc7cb80def22f385728ab655b5b
a.baydin <a.baydin@etpgpb.ru>
2020-09-14 13:25:00 +0300

    gpn_market-2585 Удалил старый чат

--- www/src/app/core/core.module.ts
+++ www/src/app/core/core.module.ts
@@ -9,7 +9,6 @@ import { BaseUrlInterceptor } from "./interceptor/base-url-interceptor";
 import { UxgModule } from "uxg";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { MessageSharedModule } from "../message/message-shared.module";
 import { WelcomeComponent } from "../pages/welcome/welcome.component";
 import { ChatSharedModule } from "../chat/chat-shared.module";
 
@@ -18,7 +17,6 @@ import { ChatSharedModule } from "../chat/chat-shared.module";
     CommonModule,
     RouterModule,
     UxgModule,
-    MessageSharedModule,
     ChatSharedModule
   ],
   exports: [NavComponent, WelcomeComponent],
