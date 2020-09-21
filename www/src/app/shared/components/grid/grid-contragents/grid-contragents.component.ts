bb51849eaf8508485da172e24c236fb2bff313c3
Alisia Mikhaylova <a.mikhaylova@etpgpb.ru>
2020-09-21 19:06:30 +0300

    gpn_market-2579 правка кодстайл

--- www/src/app/shared/components/grid/grid-contragents/grid-contragents.component.ts
+++ www/src/app/shared/components/grid/grid-contragents/grid-contragents.component.ts
@@ -17,7 +17,12 @@ import { FormControl } from "@angular/forms";
 })
 export class GridContragentsComponent implements AfterViewInit, OnChanges, AfterViewChecked {
   @ViewChild('gridRow') gridRow: ElementRef;
-  @ViewChild('gridCommonParams') set gridCommonParams(gridCommonParams) {gridCommonParams ? this.openCommonParams.emit() : null};
+  @ViewChild('gridCommonParams')
+  set gridCommonParameters(gridCommonParams) {
+    if (gridCommonParams) {
+      this.openCommonParams.emit();
+    }
+  }
   @Input() gridRows: ElementRef[] | QueryList<ElementRef>;
   @Input() suppliers: GridSupplier[];
   @Input() positionCell: boolean;
