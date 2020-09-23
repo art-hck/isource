8a37b94f71b95d64d81d1fefc686a2ce31a4f596
Alisia Mikhaylova <a.mikhaylova@etpgpb.ru>
2020-09-23 16:18:10 +0300

    gpn_market-2675 Скрыто поле Изготовитель при создании ТП

--- www/src/app/request/back-office/components/proposal-form-manufacturer/proposal-form-manufacturer.component.ts
+++ www/src/app/request/back-office/components/proposal-form-manufacturer/proposal-form-manufacturer.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, ControlValueAccessor, FormArray, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, Validators } from "@angular/forms";
+import { proposalManufacturerValidator } from "./proposal-form-manufacturer.validator";
 
 @Component({
   selector: 'app-request-proposal-form-manufacturer',
@@ -14,6 +15,7 @@ import { AbstractControl, ControlValueAccessor, FormArray, FormBuilder, FormGrou
 export class ProposalFormManufacturerComponent implements OnInit, ControlValueAccessor {
   @Output() cancel = new EventEmitter();
   @Input() disabledFn: (item) => boolean;
+  @Input() showManufacturer = true;
   public onTouched: (value) => void;
   public onChange: (value) => void;
   public form: FormGroup;
@@ -51,12 +53,14 @@ export class ProposalFormManufacturerComponent implements OnInit, ControlValueAc
     const form = this.fb.group({
       position: position,
       manufacturingName: [manufacturingName || position.name, Validators.required],
-      manufacturer: [manufacturer, Validators.required]
+      manufacturer: [manufacturer]
     });
 
     if (this.disabledFn && this.disabledFn({ position })) {
       form.disable();
     }
+
+    form.get('manufacturer').setValidators(this.showManufacturer ? [Validators.required] : null);
 
     return form;
   }
@@ -71,6 +75,11 @@ export class ProposalFormManufacturerComponent implements OnInit, ControlValueAc
   }
 
   get pristineCount() {
-    return this.formPositions.controls.filter(c => !c.get('manufacturingName').value || !c.get('manufacturer').value).length;
+    if(this.showManufacturer) {
+      return this.formPositions.controls.filter(c => !c.get('manufacturingName').value || !c.get('manufacturer').value).length;
+    } else {
+      return this.formPositions.controls.filter(c => !c.get('manufacturingName').value).length;
+    }
+
   }
 }
