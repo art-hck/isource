ad831921641484db0a20b931f70f7c602fc3becc
a.baydin <a.baydin@etpgpb.ru>
2020-10-12 17:53:54 +0300

    gpn_market-2835 Рефакторинг фильтров страницы соглашений

--- www/src/app/agreements/customer/components/agreements/agreements.component.ts
+++ www/src/app/agreements/customer/components/agreements/agreements.component.ts
@@ -1,47 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Select, Store } from "@ngxs/store";
-import { Observable } from "rxjs";
+import { combineLatest, Observable, Subject } from "rxjs";
 import { StateStatus } from "../../../../request/common/models/state-status";
 import { AgreementListState } from "../../states/agreement-list.state";
 import { AgreementListActions } from "../../actions/agreement-list.actions";
-import Fetch = AgreementListActions.Fetch;
-import { map } from "rxjs/operators";
+import { debounceTime, map, startWith, takeUntil, tap } from "rxjs/operators";
 import { ActivatedRoute, Router } from "@angular/router";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
 import { Agreement } from "../../../common/models/Agreement";
 import { AgreementAction } from "../../../back-office/enum/agreement-action";
 import { AgreementActionFilters } from "../../dictionaries/agreement-action-label";
 import { AgreementsListFilter } from "../../../common/models/agreements-list/agreements-list-filter";
 import moment from "moment";
+import Fetch = AgreementListActions.Fetch;
 
 @Component({
   templateUrl: './agreements.component.html',
   styleUrls: ['./agreements.component.scss']
 })
-export class AgreementsComponent implements OnInit {
+export class AgreementsComponent implements OnInit, OnDestroy {
   @Select(AgreementListState.agreements) agreements$: Observable<Agreement[]>;
   @Select(AgreementListState.status) status$: Observable<StateStatus>;
   @Select(AgreementListState.totalCount) totalCount$: Observable<number>;
 
   filters: AgreementsListFilter = {};
-  filterOpened = false;
 
-  readonly pageSize = 10;
-  readonly actions: {type: AgreementAction[], label: string}[] = AgreementActionFilters;
   pages$: Observable<number>;
-
-  form = new FormGroup({
-    actions: new FormControl(null)
-  });
+  readonly pageSize = 10;
+  readonly actions: { type: AgreementAction[], label: string }[] = AgreementActionFilters;
+  readonly form = this.fb.group({ actions: null, numberOrName: '', issuedDateFrom: null, issuedDateTo: null, contragents: [[]] });
+  readonly destroy$ = new Subject();
 
   get activeFilters() {
-    return this.filters && Object.entries(this.filters)
-      .filter(([k, v]: [keyof AgreementsListFilter, any]) => {
-        return k !== 'actions' && v instanceof Array && v.length > 0 || !(v instanceof Array) && v;
-      });
+    return Object.entries(this.form.controls).filter(([k, c]) => k !== 'actions' && c.dirty);
   }
 
   constructor(
+    private fb: FormBuilder,
     public store: Store,
     public route: ActivatedRoute,
     public router: Router
@@ -49,27 +44,28 @@ export class AgreementsComponent implements OnInit {
 
   ngOnInit() {
     this.pages$ = this.route.queryParams.pipe(map(params => +params["page"]));
-    this.form.valueChanges.subscribe(
-      () => {
-        this.router.navigate(["."], {relativeTo: this.route, queryParams: null});
-        this.filters.actions = this.form.value.actions;
-        this.store.dispatch(new Fetch(this.filters, 0, this.pageSize));
-      }
-    );
+
+    combineLatest([
+      this.form.valueChanges.pipe(
+        tap(() => this.router.navigate(["."], { relativeTo: this.route, queryParams: null })),
+        startWith({}),
+        debounceTime(300),
+      ),
+      this.pages$
+    ]).pipe(takeUntil(this.destroy$)).subscribe(([filters, page]) => {
+      this.store.dispatch(new Fetch({
+          ...filters,
+          issuedDateFrom: filters.issuedDateFrom ? moment(filters.issuedDateFrom, 'DD.MM.YYYY').format('YYYY-MM-DD') : null,
+          issuedDateTo: filters.issuedDateTo ? moment(filters.issuedDateTo, 'DD.MM.YYYY').format('YYYY-MM-DD') : null
+        },
+        (page ? page - 1 : 0) * this.pageSize,
+        this.pageSize
+      ));
+    });
   }
 
-  loadPage(page: number) {
-    this.store.dispatch(new Fetch(this.filters, (page - 1) * this.pageSize, this.pageSize));
-  }
-
-  filterList(filters: AgreementsListFilter) {
-    this.filters = filters;
-
-    this.filters.actions = (this.form.get('actions').value?.length > 0) ? this.form.get('actions').value : null;
-    this.filters.issuedDateFrom = filters.issuedDateFrom !== '' ? moment(moment(filters.issuedDateFrom, 'DD.MM.YYYY')).format('YYYY-MM-DD') : null;
-    this.filters.issuedDateTo = filters.issuedDateTo !== '' ? moment(moment(filters.issuedDateTo, 'DD.MM.YYYY')).format('YYYY-MM-DD') : null;
-    this.filters.contragents = filters.contragents.length > 0 ? filters.contragents : null;
-
-    this.store.dispatch(new Fetch(this.filters, 0, this.pageSize));
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
