8fd8d2418972be022f5febfb40b3758fc2b15b1a
Исраелян Карен <id12102180@ya.ru>
2020-10-13 16:51:18 +0300

    gpn_market-2853 Запретил отменять позицию для Зака и БО, если она участвует в процедуре

--- www/src/app/request/common/components/request/request.component.ts
+++ www/src/app/request/common/components/request/request.component.ts
@@ -89,8 +89,8 @@ export class RequestComponent implements OnChanges {
     return positions.some(position => position.status === status);
   }
 
-  someOfPositionsAreInProcedure(positions: RequestPosition[]): boolean {
-    return positions.some(position => position.isInProcedure === true);
+  someOfPositionsAreInProcedure(): boolean {
+    return this.checkedPositions.some(position => position.isInProcedure === true);
   }
 
   everyPositionIsNotDraftEntity(positions: RequestPosition[]): boolean {
