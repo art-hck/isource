73d0501f730aeaf59a83467d4da8fdc3ff2a2c85
a.baydin <a.baydin@etpgpb.ru>
2020-10-28 15:14:32 +0300

    gpn_market-2975

---
+++ www/src/app/request/back-office/services/mock.ts
@@ -0,0 +1,98 @@
+import { ProposalGroup } from "../../common/models/proposal-group";
+import { PositionStatus } from "../../common/enum/position-status";
+import { TechnicalCommercialProposalStatus } from "../../common/enum/technical-commercial-proposal-status";
+
+export const ProposalGroups: ProposalGroup[] = [{
+  "id": "d807999f-daaa-40d8-b5ef-e76eb10a235a",
+  "requestId": "4a3d6dc4-f833-43af-9b6c-5ac60d878af8",
+  "name": "\u0417\u0430\u043a\u0443\u043f\u043a\u0430 1",
+  "requestPositions": [{
+    "id": "d9117409-8ddb-4a4f-b791-187129b4689f",
+    "name": "\u041f\u043e\u0441\u0443\u0434\u043e\u043c\u043e\u0435\u0447\u043d\u0430\u044f \u043c\u0430\u0448\u0438\u043d\u0430 Bosch SPS 25CI07 E",
+    "status": null,
+    "number": 1,
+    "history": null
+  }, {
+    "id": "2f5e509b-71df-4b9d-ae73-f16b10bbdc02",
+    "name": "\u041c\u0435\u0434\u0438\u0430\u043f\u043b\u0435\u0435\u0440 Xiaomi Mi Box S",
+    "status": PositionStatus.DELIVERY,
+    "number": 2,
+    "history": null
+  }],
+  "createdDate": "2020-09-16T21:57:13+00:00",
+  "status": TechnicalCommercialProposalStatus.SENT_TO_EDIT,
+  "statusLabel": "\u041d\u0430 \u0434\u043e\u0440\u0430\u0431\u043e\u0442\u043a\u0435"
+}, {
+  "id": "40703e7b-43aa-48ee-b7e9-3b143b1b8292",
+  "requestId": "4a3d6dc4-f833-43af-9b6c-5ac60d878af8",
+  "name": "123123",
+  "requestPositions": [{
+    "id": "2ced9e51-dd99-41db-ab41-1c86b804d5cb",
+    "name": "\u0428\u0442\u0443\u043a\u0430\u0442\u0443\u0440\u043a\u0430 \u0433\u0438\u043f\u0441\u043e\u0432\u0430\u044f Knauf \u0420\u043e\u0442\u0431\u0430\u043d\u0434 30 \u043a\u0433",
+    "status": PositionStatus.DELIVERY,
+    "number": 3,
+    "history": null
+  }, {
+    "id": "a6a2fba4-d8d6-4f22-83cd-23606a0ccacc",
+    "name": "\u0413\u0432\u043e\u0437\u0434\u044c \u0441\u0442\u0440\u043e\u0438\u0442\u0435\u043b\u044c\u043d\u044b\u0439 4x100, 1 \u043a\u0433",
+    "status": PositionStatus.DELIVERY,
+    "number": 4,
+    "history": null
+  }, {
+    "id": "e660c6d3-058c-49be-8465-075b5ed13b62",
+    "name": "\u041f\u043e\u0441\u0443\u0434\u043e\u043c\u043e\u0435\u0447\u043d\u0430\u044f \u043c\u0430\u0448\u0438\u043d\u0430 Bosch SPS 25CI07 E",
+    "status": PositionStatus.DELIVERY,
+    "number": 5,
+    "history": null
+  }, {
+    "id": "adcaa739-6fcb-475e-89bf-19fa0eae50d5",
+    "name": "\u0421\u043c\u0430\u0440\u0442\u0444\u043e\u043d HUAWEI P40 Pro",
+    "status": PositionStatus.DELIVERY,
+    "number": 6,
+    "history": null
+  }, {
+    "id": "7c63daa2-3015-4219-9f3e-65b1b9378d8d",
+    "name": "\u041a\u043e\u043c\u043f\u043b\u0435\u043a\u0442 \u043e\u043f\u043e\u0440 \u043a \u0434\u0443\u0433\u0430\u043c THULE Rapid System 754",
+    "status": PositionStatus.DELIVERY,
+    "number": 9,
+    "history": null
+  }, {
+    "id": "77cebfaf-97d6-4be3-bdf6-06e681c1f163",
+    "name": "\u041c\u043e\u0440\u043e\u0437\u0438\u043b\u044c\u043d\u0438\u043a Gorenje FN 6191 CX",
+    "status": PositionStatus.DELIVERY,
+    "number": 10,
+    "history": null
+  }],
+  "createdDate": "2020-09-17T23:09:01+00:00",
+  "status": TechnicalCommercialProposalStatus.SENT_TO_REVIEW,
+  "statusLabel": "\u041d\u0430 \u0440\u0430\u0441\u0441\u043c\u043e\u0442\u0440\u0435\u043d\u0438\u0438"
+}, {
+  "id": "7630d2b8-7081-417b-806f-5270c06915da",
+  "requestId": "4a3d6dc4-f833-43af-9b6c-5ac60d878af8",
+  "name": "\u0417\u0430\u043a\u0443\u043f\u043a\u0430 32",
+  "requestPositions": [{
+    "id": "4fec4f67-8267-42c4-a32c-dbb6147feca2",
+    "name": "\u0410\u043d\u0442\u0438\u0441\u0435\u043f\u0442\u0438\u0447\u0435\u0441\u043a\u043e\u0435 \u0441\u0440\u0435\u0434\u0441\u0442\u0432\u043e \u0434\u043b\u044f \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438 \u0440\u0443\u043a \u0438 \u0441\u0442\u0443\u043f\u043d\u0435\u0439 Domix Green Professional \u0422\u043e\u0442\u0430\u043b\u0434\u0435\u0437 \u0433\u0435\u043b\u044c",
+    "status": null,
+    "number": 7,
+    "history": null
+  }],
+  "createdDate": "2020-09-17T13:06:28+00:00",
+  "status": TechnicalCommercialProposalStatus.DRAFT,
+  "statusLabel": "\u0427\u0435\u0440\u043d\u043e\u0432\u0438\u043a"
+}, {
+  "id": "af84c213-dcc7-4260-985a-4b06b4f575a2",
+  "requestId": "4a3d6dc4-f833-43af-9b6c-5ac60d878af8",
+  "name": "\u0417\u0430\u043a\u0443\u043f\u043a\u0430 21",
+  "requestPositions": [{
+    "id": "147f9639-9584-4cb3-8292-81408f9c2f84",
+    "name": "\u041f\u0435\u0440\u0447\u0430\u0442\u043a\u0438 Aviora \u0412\u0438\u043d\u0438\u043b\u043e\u0432\u044b\u0435 \u043d\u0435\u043e\u043f\u0443\u0434\u0440\u0435\u043d\u043d\u044b\u0435",
+    "status": null,
+    "number": 8,
+    "history": null
+  }],
+  "createdDate": "2020-09-16T22:30:24+00:00",
+  "status": TechnicalCommercialProposalStatus.DRAFT,
+  "statusLabel": "\u0427\u0435\u0440\u043d\u043e\u0432\u0438\u043a"
+}];
+
