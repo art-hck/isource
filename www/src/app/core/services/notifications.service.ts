80ede514a18a93ec52fcfa7b790cda0f5c1a2d95
Исраелян Карен <id12102180@ya.ru>
2020-11-12 18:38:12 +0300

    gpn_market-3065 Доработки по нотификациям

--- www/src/app/core/services/notifications.service.ts
+++ www/src/app/core/services/notifications.service.ts
@@ -58,9 +58,9 @@ export class NotificationsService {
     this.onNotificationRead().subscribe(() => this.unreadCount());
   }
 
-  getNotifications() {
+  getNotifications(limit) {
     const url = `#notifications#`;
-    return this.api.get<Notifications>(url, {params: {limit: '30', channel: '1'}}).pipe(map(
+    return this.api.get<Notifications>(url, {params: {limit: limit, channel: '1'}}).pipe(map(
       notifications => {
         notifications.items = notifications.items.map(item => {
           item.body = JSON.parse(item.body as string);
