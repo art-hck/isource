dfd1058787f7eb2b5276bd0043ac2a04f5912594
a.baydin <a.baydin@etpgpb.ru>
2020-12-21 15:01:27 +0300

    gpn_market-3419 Карточка заявки. К позиции имеется возможность прикрепить любой элемент интерфейс...

--- www/src/app/shared/directives/upload-file-drag.directive.ts
+++ www/src/app/shared/directives/upload-file-drag.directive.ts
@@ -10,21 +10,21 @@ export class UploadFileDragDirective {
   constructor(private el: ElementRef) {}
 
   @HostListener("dragover", ["$event"])
-  public onDragOver(e: Event) {
+  public onDragOver(e: Event & { dataTransfer: DataTransfer }) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.add(this.hoverClass);
   }
 
   @HostListener("dragleave", ["$event"])
-  public onDragLeave(e: Event) {
+  public onDragLeave(e: Event & { dataTransfer: DataTransfer }) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.remove(this.hoverClass);
   }
 
   @HostListener('drop', ['$event'])
-  public onDrop(e: Event | any) {
+  public onDrop(e: Event & { dataTransfer: DataTransfer }) {
     e.preventDefault();
     e.stopPropagation();
     this.el.nativeElement.classList.remove(this.hoverClass);
