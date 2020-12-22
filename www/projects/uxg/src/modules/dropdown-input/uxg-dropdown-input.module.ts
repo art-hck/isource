8392645de8a078e20832fe1e40770fb30c4aea40
a.baydin <a.baydin@etpgpb.ru>
2020-12-22 16:24:41 +0300

    gpn_market-3279 Отсутствует стрелочка в выпадающем списке

--- www/projects/uxg/src/modules/dropdown-input/uxg-dropdown-input.module.ts
+++ www/projects/uxg/src/modules/dropdown-input/uxg-dropdown-input.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { UxgDropdownInputComponent } from "./uxg-dropdown-input.component";
 import { UxgInputModule } from "../input/uxg-input.module";
 import { CommonModule } from "@angular/common";
+import { UxgIconModule } from "../icon/uxg-icon.module";
 
 
 @NgModule({
-  imports: [CommonModule, UxgInputModule],
+  imports: [CommonModule, UxgInputModule, UxgIconModule],
   declarations: [UxgDropdownInputComponent],
   exports: [UxgDropdownInputComponent],
 })
